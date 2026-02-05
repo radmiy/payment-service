cc2e5c5daf45a02e3b4697d70d9d611207963645
Dzmitry Rabykin <radmiyrdv@gmail.com>
2026-02-05 18:04:57 +0300

    added manifest files for kubernetes reformated controller

--- payment-service-app/src/main/java/com/radmiy/payment/service/app/controller/PaymentController.java
+++ payment-service-app/src/main/java/com/radmiy/payment/service/app/controller/PaymentController.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
